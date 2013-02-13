46fbb6aab2cfa9181c244f89a47bd880d7572da5
Artur Opala <opala.artur@gmail.com>
2013-02-13 16:20:41 +0100

    graph

--- src/main/scala/Graph.scala
+++ src/main/scala/Graph.scala
@@ -1,22 +1,64 @@
 import scalax.file.Path
+import scala.collection.mutable.Map
 
 object Graph {
+
+	def apply[@specialized(Int) T](nodes:(T,Seq[T])*) = new Graph(Map(nodes:_*))
 	
-	def readFromFile(path:Path):Graph = {
-		val nodes:Array[GraphNode] = (for (line <-path.lines() if !line.trim.isEmpty) yield parseNodeLine(line)).filter(_!=null).toArray
-		Graph(nodes)
+	def readFromFile(path:Path):Graph[Int] = {
+		val nodes:Seq[(Int,Seq[Int])] = (for (line <-path.lines() if !line.trim.isEmpty) yield readNode(line)).filter(_!=null).toSeq
+		Graph(Map(nodes:_*))
 	}
 	
-	private def parseNodeLine(line:String):GraphNode = {
+	private def readNode(line:String):(Int,Seq[Int]) = {
 		val tokens = line.split('\t')
 		if(tokens.length==0) return null
-		val label:Int = Integer.parseInt(tokens(0))
-		val adjacent:Array[Int] = (for (i <- 1 until tokens.length) yield  Integer.parseInt(tokens(i))).toArray
-		GraphNode(label,adjacent)
+		val key:Int = Integer.parseInt(tokens(0))
+		val adjacent:Seq[Int] = for (i <- 1 until tokens.length) yield  Integer.parseInt(tokens(i))
+		(key,adjacent)
+	}
+
+	private def mergeNodes[T](nodeMap: Map[T, Seq[T]], mergedNode: T, removedNode: T):Map[T, Seq[T]] =  {
+		//merge two adjacent lists, remove self-loops
+		val newAdjacent = (nodeMap(mergedNode) ++ nodeMap(removedNode)) filter (node => node != mergedNode && node != removedNode)
+		nodeMap -= removedNode //remove merged node
+		nodeMap -= mergedNode //remove target node
+		nodeMap transform {
+			(node, adjacent) => adjacent map {
+				case n if (n == removedNode) => mergedNode //replace all references to the merged node
+				case n => n
+			}
+		}
+		nodeMap(mergedNode) = newAdjacent
+		nodeMap
 	}
 
 }
 
-case class GraphNode(label:Int, adjacent:Array[Int])
+case class Graph[@specialized(Int) T](private val nodeMap:Map[T,Seq[T]]){
+	
+	def adjacentOf(node:T):Seq[T] = nodeMap(node)
+	def has(node:T):Boolean =  nodeMap.contains(node)
+	def nodes:Iterable[T] = nodeMap.keys
+	def nodesCount:Int = nodeMap.size
 
-case class Graph(nodes:Array[GraphNode])
+	def mergeNodes(mergedNode:T, removedNode:T):Graph[T] = {
+		assert(has(mergedNode))
+		assert(has(removedNode))
+		Graph(Graph.mergeNodes(nodeMap.clone(), mergedNode, removedNode))
+	}
+	
+	def randomCutCount:Int = {
+		val nodeMapClone = nodeMap.clone()
+		while(nodeMapClone.size>2){
+			val (node1,adjacent) = nodeMapClone.head
+			if(adjacent.size>0){
+				val j = (Math.random()*adjacent.size).asInstanceOf[Int]
+				val node2 =  adjacent(j)
+				Graph.mergeNodes(nodeMapClone,node1,node2)
+			}
+		}
+		val (_, adjacent) = nodeMapClone.head
+		adjacent.size
+	}
+}
--- src/test/scala/GraphTest.scala
+++ src/test/scala/GraphTest.scala
@@ -4,9 +4,51 @@ import scalax.file.Path
 
 class GraphTest {
 
+	val graph1 = Graph[Int](
+		0 -> Seq(1,3),
+		1 -> Seq(0,2,3),
+		2 -> Seq(1,3),
+		3 -> Seq(0,1,2)
+	)
+	
+	val graph2 = Graph.readFromFile(Path.fromString("src/main/resources/graph1.txt"))
+
 	@Test def testReader = {
-		val graph = Graph.readFromFile(Path.fromString("src/main/resources/graph1.txt"))
-		assertEquals(200,graph.nodes.size)
+		assertEquals(200,graph2.nodesCount)
+	}
+
+	@Test def testMergeNodes = {
+		val graph2 = graph1.mergeNodes(1,0)
+		assertFalse(graph2.has(0))
+		assertEquals(Set(1,2,3),graph2.nodes)
+		assertEquals(Seq(2,3,3),graph2.adjacentOf(1))
+		assertEquals(Seq(1,3),graph2.adjacentOf(2))
+		assertEquals(Seq(1,1,2),graph2.adjacentOf(3))
+		val graph3 = graph2.mergeNodes(2,1)
+		assertFalse(graph3.has(1))
+		assertEquals(Set(2,3),graph3.nodes)
+		assertEquals(Seq(3,3,3),graph3.adjacentOf(2))
+		assertEquals(Seq(2,2,2),graph3.adjacentOf(3))
+		val graph4 = graph3.mergeNodes(3,2)
+		assertFalse(graph4.has(2))
+		assertEquals(Set(3),graph4.nodes)
+		assertEquals(Seq.empty,graph4.adjacentOf(3))
+		Console.println(graph1)
+		Console.println(graph2)
+		Console.println(graph3)
+		Console.println(graph4)
+	}
+
+	@Test def testRandomCutCount = {
+		for(i <- 1 to 10){
+			Console.println(graph1.randomCutCount)
+		}
+	}
+
+	@Test def testRandomCutCount2 = {
+		for(i <- 1 to 100){
+			Console.println(graph2.randomCutCount)
+		}
 	}
 	
 }
