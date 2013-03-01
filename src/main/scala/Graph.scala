6a269c07464776c8b424b1c57973e5cc39c043dc
artur.opala <opala.artur@gmail.com>
2013-03-01 08:45:24 +0100

    graph redesign

--- src/main/scala/Graph.scala
+++ src/main/scala/Graph.scala
@@ -1,108 +1,51 @@
-import scalax.file.Path
-import collection.mutable.{ArrayBuffer, Map, Queue}
 
-class Graph[@specialized(Int) N](private val nodeMap:Map[N,Seq[N]] ){
+trait Graph[@specialized(Int) N] extends Traversable[(N,N)] {
+    def nodes: Iterable[N]
+    def adjacent: N => Traversable[N]
 
-  def adjacentOf(node:N):Seq[N] = nodeMap(node)
-  def adjacentOrUpdate(node:N,up: => Seq[N]):Seq[N] = nodeMap.getOrElseUpdate(node,up)
+    def edges: Traversable[(N,N)]
+    def reverse: Graph[N]
+}
 
-  def update(node:N,adjacent:Seq[N]) = nodeMap.put(node,adjacent)
+trait Weighted[@specialized(Int) N, @specialized(Double) V] {
+    def weight: (N,N) => V
+}
 
-  def has(node:N):Boolean =  nodeMap.contains(node)
-  def nodes:Iterable[N] = nodeMap.keys
+trait GraphBase[@specialized(Int) N] extends Graph[N] {
+    self =>
 
-  def nodesCount:Int = nodeMap.size
-  def edgesCount:Long = nodeMap.valuesIterator.foldLeft(0L){case (sum,adjacent) => sum + adjacent.size}
-
-  def mergeNodes(mergedNode:N, removedNode:N):Graph[N] = {
-    assert(has(mergedNode))
-    assert(has(removedNode))
-    new Graph(Graph.mergeNodes(nodeMap.clone(), mergedNode, removedNode))
-  }
+    override def edges: Traversable[(N,N)] = self
+    override def foreach[U](f: ((N, N)) => U) {
+        for(from <- nodes; to <- adjacent(from)) f((from,to))
+    }
+    override def reverse:Graph[N] = new GraphBase[N] {
+        override val nodes: Iterable[N] = self.nodes
+        override val adjacent: N => Traversable[N] = node => new Traversable[N]{
+            def foreach[U](f: (N) => U) {
+               for (n <- self.nodes if Graph.contains(self.adjacent(n),node)) f(n)
+            }
+        }
+        override val edges: Traversable[(N,N)] =  new Traversable[(N,N)] {
+            def foreach[U](f: ((N,N)) => U) {
+                for(from <- self.nodes; to <- self.adjacent(from)) f((to,from))
+            }
+        }
+        override val reverse = self
+    }
 }
 
 object Graph {
-
-	def apply[@specialized(Int) N](nodes:(N,Seq[N])*):Graph[N] = new Graph(Map(nodes:_*))
-	
-	def readFromAdjacentListFile(path:Path):Graph[Int] = {
-    val graph = Graph[Int]()
-    for (line <-path.lines() if !line.trim.isEmpty) {
-      val (node, adjacent) = readNodeAdjacentList(line)
-      if(adjacent!=null) graph(node) = adjacent
+    def apply[@specialized(Int) N](mappings:(N,Iterable[N])*): Graph[N] = new GraphBase[N] {
+        val map = mappings.toMap
+        def nodes: Iterable[N] = map.keys
+        def adjacent: N => Iterable[N] = map
     }
-    graph
-	}
 
-	def readFromEdgeFile(path:Path):Graph[Int] = {
-		val graph = Graph[Int]()
-		for (line <-path.lines() if !line.trim.isEmpty) {
-      val i = line.indexOf(' ')
-			val tail = line.substring(0,i).toInt
-      val head = line.substring(i+1).trim.toInt
-			val adjacent = graph.adjacentOrUpdate(tail,{new ArrayBuffer[Int]()}).asInstanceOf[ArrayBuffer[Int]] //safe
-      adjacent += head
-		}
-		graph
-	}
-	
-	private def readNodeAdjacentList(line:String):(Int,Seq[Int]) = {
-		val tokens = line.split('\t')
-		if(tokens.length==0) return null
-		val label:Int = Integer.parseInt(tokens(0))
-		val adjacent:Seq[Int] = Seq((for (i <- 1 until tokens.length) yield  Integer.parseInt(tokens(i))):_*)
-		(label,adjacent)
-	}
-
-  def randomCutCount[@specialized(Int) N](graph:Graph[N]):Int = {
-    val nodeMapClone = graph.nodeMap.clone()
-    val nodesQueue = Queue[N](randomize(Seq[N](nodeMapClone.keySet.toSeq:_*)):_*)
-    while(nodeMapClone.size>2){
-      val node1 = nodesQueue.dequeue
-      val adjacent = nodeMapClone(node1)
-      if(adjacent.size>0){
-        val j = (Math.random()*adjacent.size).asInstanceOf[Int]
-        val node2 =  adjacent(j)
-        mergeNodes(nodeMapClone,node2,node1)
-      }
+    def contains[@specialized(Int) N](col:Traversable[N],node:N):Boolean = {
+        col match {
+            case set:Set[N] => set.contains(node)
+            case _ => col exists (n => n==node)
+        }
     }
-    val (_, adjacent) = nodeMapClone.head
-    adjacent.size
-  }
-
-	private def mergeNodes[@specialized(Int) N](nodeMap: Map[N, Seq[N]], mergedNode: N, removedNode: N):Map[N, Seq[N]] =  {
-		//merge two adjacent lists, remove self-loops
-		val removedAdjacent = nodeMap(removedNode)
-    val mergedAdjacent = nodeMap(mergedNode)
-		val newAdjacent = new ArrayBuffer[N](removedAdjacent.size+mergedAdjacent.size)
-		for(node <- mergedAdjacent) {
-			if(node != removedNode) newAdjacent += node
-		}
-		for(node <- removedAdjacent) {
-			if(node != mergedNode) newAdjacent += node
-		}
-		nodeMap -= removedNode //remove node
-		nodeMap(mergedNode) = newAdjacent //set new adjacent for mergedNode
-		nodeMap transform {
-			(_, adjacent) => {
-				if (adjacent.contains(removedNode)){
-					adjacent map {
-						case n if n==removedNode => mergedNode
-						case n => n
-					}
-				} else {
-					adjacent
-				}
-			}
-		}
-		nodeMap
-	}
-	
-	def randomize[N](seq:Seq[N]):Seq[N] =  {
-		seq
-			.map(item => (Math.random(),item))
-			.sortBy{case (priority,_) => priority}
-			.map{case (_,item) => item}
-	}
-
 }
+
--- src/test/scala/GraphTest.scala
+++ src/test/scala/GraphTest.scala
@@ -1,71 +1,32 @@
-import org.junit.Assert._
-import org.junit.Test
-import scalax.file.Path
-import collection.mutable.{Map, Seq}
+import org.scalatest.FunSpec
 
-class GraphTest {
+class GraphTest extends FunSpec {
 
-	val graph1 = Graph[Int](
-		0 -> Seq(1,3),
-		1 -> Seq(0,2,3),
-		2 -> Seq(1,3),
-		3 -> Seq(0,1,2)
-	)
+    describe("Graph"){
 
-	val graph2 = Graph[Int](
-		0 -> Seq(1,2,3),
-		1 -> Seq(0,2,3),
-		2 -> Seq(0,1,3),
-		3 -> Seq(0,1,2)
-	)
-	
-	val graph = Graph.readFromAdjacentListFile(Path.fromString("src/main/resources/graph1.txt"))
+        val graph = Graph(
+            1 -> Seq(2,3),
+            2 -> Seq(3),
+            3 -> Seq(4),
+            4 -> Seq()
+        )
 
-	@Test def testReader = {
-		assertEquals(200,graph.nodesCount)
-	}
+        it("should have nodes and edges") {
+            assert(graph.nodes.size==4, "graph nodes count should be 4")
+            assert(graph.edges.size==4, "graph edges count should be 4")
+        }
+        it("should have reverse graph") {
+            val reverse = graph.reverse
+            val redges = reverse.edges.toSeq
+            val reversed2 = reverse.reverse
+            val radjacentOf1 = reverse.adjacent(1).toSeq
+            val radjacentOf4 = reverse.adjacent(4).toSeq
+            assert(reverse.nodes.size == 4, "reversed graph nodes count should be 4")
+            assert(redges.size == 4, "reversed graph edges count should be 4")
+            assert(reversed2 == graph, "twice reversed graph should be the same")
+            assert(radjacentOf1.isEmpty)
+            assert(radjacentOf4 == Seq(3))
+        }
+    }
 
-	@Test def testMergeNodes = {
-		val graph2 = graph1.mergeNodes(1,0)
-		assertFalse(graph2.has(0))
-		assertEquals(Set(1,2,3),graph2.nodes)
-		assertEquals(Seq(2,3,3),graph2.adjacentOf(1))
-		assertEquals(Seq(1,3),graph2.adjacentOf(2))
-		assertEquals(Seq(1,1,2),graph2.adjacentOf(3))
-		val graph3 = graph2.mergeNodes(2,1)
-		assertFalse(graph3.has(1))
-		assertEquals(Set(2,3),graph3.nodes)
-		assertEquals(Seq(3,3,3),graph3.adjacentOf(2))
-		assertEquals(Seq(2,2,2),graph3.adjacentOf(3))
-		val graph4 = graph3.mergeNodes(3,2)
-		assertFalse(graph4.has(2))
-		assertEquals(Set(3),graph4.nodes)
-		assertEquals(Seq.empty,graph4.adjacentOf(3))
-		Console.println(graph1)
-		Console.println(graph2)
-		Console.println(graph3)
-		Console.println(graph4)
-	}
-
-	@Test def testRandomCutCount = {
-		for(i <- 1 to 10){
-			Console.println(Graph.randomCutCount(graph1))
-		}
-	}
-
-	@Test def findMinCutCount = {
-		var count = Integer.MAX_VALUE
-		for(i <- 1 to 20){
-			count = Math.min(Graph.randomCutCount(graph),count)
-		}
-		Console.println(count)
-	}
-
-  @Test def readSCCGraph = {
-    val graph = Graph.readFromEdgeFile(Path.fromString("src/main/resources/SCC.txt"))
-    assert(graph!=null)
-    Console.println(graph.nodesCount)
-    Console.println(graph.edgesCount)
-  }
-	
 }
