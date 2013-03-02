ab2ace68bf27eb1733e60fb2340042cea7177209
artur.opala <opala.artur@gmail.com>
2013-03-02 15:59:44 +0100

    graph redesign

--- src/main/scala/Graph.scala
+++ src/main/scala/Graph.scala
@@ -4,10 +4,8 @@ import scalax.file.Path
 trait Graph[@specialized(Int) N] extends Traversable[(N,N)] {
     def nodes: Iterable[N]
     def adjacent: N => Traversable[N]
-
     def edges: Traversable[(N,N)]
     def reverse: Graph[N]
-
 	def nodesCount:Int
 	def edgesCount:Long
 }
@@ -16,47 +14,40 @@ trait Weighted[@specialized(Int) N, @specialized(Double) V] {
     def weight: (N,N) => V
 }
 
-trait GraphBase[@specialized(Int) N] extends Graph[N] {
+trait GenericGraph[@specialized(Int) N] extends Graph[N] {
 	self =>
+    override def edges: Traversable[(N,N)] = this
+    override def foreach[U](f: ((N, N)) => U):Unit = for(from <- nodes; to <- adjacent(from)) f((from,to))
+    override lazy val reverse:Graph[N] = new GenericReverseGraph[N](self)
+    override def nodesCount: Int = nodes.size
+    override def edgesCount: Long = nodes.foldLeft(0L){case (sum,node) => sum + adjacent(node).size}
+}
 
-    override def edges: Traversable[(N,N)] = self
-    override def foreach[U](f: ((N, N)) => U) {
-        for(from <- nodes; to <- adjacent(from)) f((from,to))
-    }
-    override def reverse:Graph[N] = new GraphBase[N] {
-        override def nodes: Iterable[N] = self.nodes
-        override val adjacent: N => Traversable[N] = node => new Traversable[N]{
-            def foreach[U](f: (N) => U) {
-               for (n <- self.nodes if (self.adjacent(n) match {
-		            case set:Set[N] => set contains node
-		            case col => col exists (_ == node)
-	            })) f(n)
-            }
-	        def contains(col:Traversable[N],node:N):Boolean = {
-		        col match {
-			        case set:Set[N] => set.contains(node)
-			        case _ => col exists (n => n==node)
-		        }
-	        }
+class GenericReverseGraph[@specialized(Int) N](origin: Graph[N]) extends Graph[N] {
+    override def nodes: Iterable[N] = origin.nodes
+    override val adjacent: N => Traversable[N] = node => new Traversable[N]{
+        def foreach[U](f: (N) => U) {
+            for (n <- origin.nodes if (origin.adjacent(n) match {
+                case set:Set[N] => set contains node
+                case col => col exists (_ == node)
+            })) f(n)
         }
-        override val edges: Traversable[(N,N)] =  new Traversable[(N,N)] {
-            def foreach[U](f: ((N,N)) => U) {
-                for(from <- self.nodes; to <- self.adjacent(from)) f((to,from))
-            }
-        }
-        override val reverse = self
     }
-
-	def nodesCount: Int = nodes.size
-	def edgesCount: Long = nodes.foldLeft(0L){case (sum,node) => sum + adjacent(node).size}
+    override def edges: Traversable[(N,N)] =  this
+    override def foreach[U](f: ((N, N)) => U):Unit = for(from <- origin.nodes; to <- origin.adjacent(from)) f((to,from))
+    override val reverse = origin
+    override def nodesCount: Int = origin.nodesCount
+    override def edgesCount: Long = origin.edgesCount
 }
 
 object Graph {
-    def apply[@specialized(Int) N](mappings:(N,Iterable[N])*): Graph[N] = new GraphBase[N] {
+
+    def apply[@specialized(Int) N](mappings:(N,Iterable[N])*): Graph[N] = new GenericGraph[N] {
         val map = mappings.toMap
-        def nodes: Iterable[N] = map.keys
-        def adjacent: N => Iterable[N] = map
+        val nodes: Iterable[N] = map.keys
+        val adjacent: N => Iterable[N] = map
     }
+
 	def readFromEdgeListFile(path:Path):Graph[Int] = {
 		val nodeMap = scala.collection.mutable.Map[Int,scala.collection.mutable.ArrayBuffer[Int]]()
 		for (line <-path.lines() if !line.trim.isEmpty) {
@@ -66,9 +57,9 @@ object Graph {
 			val adjacent = nodeMap.getOrElseUpdate(tail,{scala.collection.mutable.ArrayBuffer[Int]()})
 			adjacent += head
 		}
-		new GraphBase[Int] {
+		new GenericGraph[Int] {
 			val nodes: Iterable[Int] = nodeMap.keys
-			def adjacent: (Int) => Traversable[Int] = nodeMap
+			val adjacent: (Int) => Traversable[Int] = nodeMap
 		}
 	}
 }
