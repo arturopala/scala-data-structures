ba9556330774c7ae0b39e3be0c2f93b1e0abadf8
artur.opala <opala.artur@gmail.com>
2013-03-04 00:25:37 +0100

    scc

--- src/main/scala/Graph.scala
+++ src/main/scala/Graph.scala
@@ -1,5 +1,8 @@
+import collection.mutable
+import com.sun.org.apache.xalan.internal.lib.NodeInfo
 import scala.specialized
 import scalax.file.Path
+import scala.collection.mutable.{Stack,ArrayBuffer}
 
 trait Graph[@specialized(Int) N] extends Traversable[(N,N)] {
     def nodes: Iterable[N]
@@ -18,7 +21,7 @@ trait GenericGraph[@specialized(Int) N] extends Graph[N] {
 	self =>
     override def edges: Traversable[(N,N)] = this
     override def foreach[U](f: ((N, N)) => U):Unit = for(from <- nodes; to <- adjacent(from)) f((from,to))
-    override lazy val reverse:Graph[N] = new GenericReverseGraph[N](self)
+    override def reverse:Graph[N] = new GenericReverseGraph[N](self)
     override def nodesCount: Int = nodes.size
     override def edgesCount: Long = nodes.foldLeft(0L){case (sum,node) => sum + adjacent(node).size}
 }
@@ -40,27 +43,110 @@ class GenericReverseGraph[@specialized(Int) N](origin: Graph[N]) extends Graph[N
     override def edgesCount: Long = origin.edgesCount
 }
 
+class MutableMapGraph[@specialized(Int) N] extends GenericGraph[N] {
+    protected val map = scala.collection.mutable.Map[N,ArrayBuffer[N]]()
+    override def nodes:Iterable[N] =  map.keys
+    override val adjacent: N => ArrayBuffer[N] = map.withDefaultValue(ArrayBuffer.empty[N])
+
+    def add(edge: (N,N)):MutableMapGraph[N] = {
+        map.getOrElseUpdate(edge._1,{ArrayBuffer[N]()}) += (edge._2); this
+    }
+    def add(edges:Traversable[(N,N)]):MutableMapGraph[N] = {
+        for (edge <- edges) add(edge); this
+    }
+    def addReverse(edge: (N,N)):MutableMapGraph[N] = {
+        map.getOrElseUpdate(edge._2,{ArrayBuffer[N]()}) += (edge._1); this
+    }
+    def addReverse(edges:Traversable[(N,N)]):MutableMapGraph[N] = {
+        for (edge <- edges) addReverse(edge); this
+    }
+}
+
 object Graph {
 
     def apply[@specialized(Int) N](mappings:(N,Iterable[N])*): Graph[N] = new GenericGraph[N] {
-        val map = mappings.toMap
+        private val map = mappings.toMap
         val nodes: Iterable[N] = map.keys
-        val adjacent: N => Iterable[N] = map
+        val adjacent: N => Traversable[N] = map.withDefaultValue(Traversable.empty[N])
     }
 
-	def readFromEdgeListFile(path:Path):Graph[Int] = {
-		val nodeMap = scala.collection.mutable.Map[Int,scala.collection.mutable.ArrayBuffer[Int]]()
-		for (line <-path.lines() if !line.trim.isEmpty) {
-			val i = line.indexOf(' ')
-			val tail = line.substring(0,i).toInt
-			val head = line.substring(i+1).trim.toInt
-			val adjacent = nodeMap.getOrElseUpdate(tail,{scala.collection.mutable.ArrayBuffer[Int]()})
-			adjacent += head
-		}
-		new GenericGraph[Int] {
-			val nodes: Iterable[Int] = nodeMap.keys
-			val adjacent: (Int) => Traversable[Int] = nodeMap
-		}
+    def apply[@specialized(Int) N](edges:Traversable[(N,N)]): Graph[N] = new MutableMapGraph[N]().add(edges)
+
+	def readFromEdgeListFile(path:Path, reversed:Boolean = false):Graph[Int] = {
+        val edges = path.lines().view map (line => {
+            val i = line.indexOf(' ')
+            val tail = line.substring(0,i).toInt
+            val head = line.substring(i+1).trim.toInt
+            if (reversed) (head,tail) else (tail,head)
+        })
+        Graph(edges)
 	}
+
+    def reverse[N](graph:Graph[N]):Graph[N] =  new MutableMapGraph[N](){
+        override lazy val reverse = graph
+    }.addReverse(graph)
+
+    trait DfsLoopObserver[N] {
+        def beforeOuter(node:N) {}
+        def beforeInner(node:N) {}
+        def afterInner(node:N) {}
+    }
+
+    class SccNodeInfo[N] {
+        var leader:Option[N] = None
+        var time:Int = 0
+    }
+
+    def scc[@specialized(Int) N](graph:Graph[N]): Seq[(N,Iterable[N])] = {
+        //first pass
+        val reversed: Graph[N] = Graph.reverse(graph)
+        val attributes = mutable.Map[N,SccNodeInfo[N]]()
+        def attrOf(node:N):SccNodeInfo[N] = attributes.getOrElseUpdate(node,{new SccNodeInfo[N]})
+        var t:Int = 0
+        var s: Option[N] = None
+        val observer1 = new DfsLoopObserver[N] {
+            override def afterInner(node:N) {
+                t = t + 1
+                attrOf(node).time = t
+            }
+        }
+        dfs(reversed,observer1)(reversed.nodes)
+        // second pass
+        val ordered = (attributes map {case (node,attr) => (node,attr.time)}).toSeq sortBy {case (_,time) => -time} map {case (node,_) => node}
+        val observer2 = new DfsLoopObserver[N] {
+            override def beforeOuter(node:N) {
+                s = Some(node)
+            }
+            override def beforeInner(node:N) {
+                attrOf(node).leader = s
+            }
+        }
+        dfs(graph,observer2)(ordered)
+        //result
+        val result = (attributes groupBy {case (_,attr) => attr.leader.get}).toSeq sortBy {case (_,map) => -map.size} map {case (n,map) => (n,map.keys)}
+        result
+    }
+
+    def dfs[@specialized(Int) N](graph:Graph[N], observer: DfsLoopObserver[N])(nodes:Iterable[N] = graph.nodes):Unit = {
+        val explored = mutable.HashSet[N]()
+        for (node <- nodes){
+            if (!(explored contains node)){
+                observer beforeOuter (node)
+                dfs(graph,node,observer,explored)
+            }
+        }
+    }
+
+    def dfs[@specialized(Int) N](graph:Graph[N],node:N, observer: DfsLoopObserver[N], explored:mutable.HashSet[N]):Unit = {
+        if (!(explored contains node)){
+            explored add node
+            observer beforeInner node
+            val next = graph.adjacent(node) filterNot explored
+            if (!next.isEmpty) next foreach {n =>
+                dfs(graph,n,observer,explored)
+            }
+            observer afterInner node
+        }
+    }
 }
 
--- src/test/scala/GraphTest.scala
+++ src/test/scala/GraphTest.scala
@@ -6,35 +6,69 @@ class GraphTest extends FunSpec {
 
     describe("Graph"){
 
-        val graph = Graph(
+        val graph1 = Graph(
             1 -> Seq(2,3),
             2 -> Seq(3),
             3 -> Seq(4),
             4 -> Seq()
         )
 
+        val graph2 = Graph(
+            1 -> Seq(2,3,6),
+            2 -> Seq(3),
+            3 -> Seq(4),
+            4 -> Seq(5,4),
+            5 -> Seq(1,3),
+            6 -> Seq(7,10),
+            7 -> Seq(8),
+            8 -> Seq(9,10),
+            9 -> Seq(6),
+            10 -> Seq(6,7),
+            11 -> Seq(12,7),
+            12 -> Seq(13),
+            13 -> Seq(11)
+        )
+
         it("should have nodes and edges") {
-            assert(graph.nodes.size==4, "graph nodes count should be 4")
-            assert(graph.edges.size==4, "graph edges count should be 4")
+            assert(graph1.nodes.size==4, "graph nodes count should be 4")
+            assert(graph1.edges.size==4, "graph edges count should be 4")
         }
         it("should have reverse graph") {
-            val reverse = graph.reverse
+            val reverse = graph1.reverse
             val redges = reverse.edges.toSeq
             val reversed2 = reverse.reverse
             val radjacentOf1 = reverse.adjacent(1).toSeq
             val radjacentOf4 = reverse.adjacent(4).toSeq
             assert(reverse.nodes.size == 4, "reversed graph nodes count should be 4")
             assert(redges.size == 4, "reversed graph edges count should be 4")
-            assert(reversed2 == graph, "twice reversed graph should be the same")
+            assert(reversed2 == graph1, "twice reversed graph should be the same")
             assert(radjacentOf1.isEmpty)
             assert(radjacentOf4 == Seq(3))
         }
-	    it("should read SSC graph") {
-		    val graph = Graph.readFromEdgeListFile(Path.fromString("src/main/resources/SCC.txt"))
-		    assert(graph!=null)
-		    Console.println(graph.nodesCount)
-		    Console.println(graph.edgesCount)
-	    }
+        it("should search graph with dfs") {
+            val graph = graph2
+            var counter = 0
+            Graph.dfs(graph,new Graph.DfsLoopObserver[Int] {
+                override def beforeInner(node:Int) {
+                    counter = counter + 1
+                }
+            })(graph.nodes)
+            assert(counter==graph.nodesCount)
+        }
+        it("should search graph with scc") {
+            val graph = graph2
+            val result = Graph.scc(graph)
+            for((n,col) <- result.take(100)) Console.println(col.size+":"+col)
+            assert(result.size==3)
+        }
+        it("should read SSC graph") {
+            val graph = Graph.readFromEdgeListFile(Path.fromString("src/main/resources/SCC.txt"))
+            assert(graph!=null)
+            Console.println(graph.nodesCount)
+            Console.println(graph.edgesCount)
+            val result = Graph.scc(graph)
+            for((n,col) <- result.take(100)) Console.println(col.size)
+        }
     }
 
 }
