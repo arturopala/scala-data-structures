9f6226f5826ff3087b0216a5ded49ab99278893f
opala.artur@gmail.com <Telesfor1928>
2013-03-15 09:25:25 +0100

    heap

--- src/main/scala/Heap.scala
+++ src/main/scala/Heap.scala
@@ -1,87 +1,120 @@
 import annotation.tailrec
-import collection.mutable.ArrayBuffer
+import collection.mutable.{Buffer, ArrayBuffer, Map}
 
-/** The (binary) heap data structure is an array object that can be viewed as a nearly complete binary tree */
-trait Heap[@specialized(Int,Long,Double) N] {
-self =>	
+trait Heap[@specialized(Int, Long, Double) N] {
+    def head: N
+    def size: Int
+    def extract: Option[N]
+    def insert(elem: N): Unit
+    def remove(elem: N): Unit
+    def refresh(elem: N): Unit
+}
 
-	val compare:(N,N) => Boolean
-	
-	private val A = ArrayBuffer[N]()
-	private var count: Int = 0
-	
-	final def apply(i:Int): N = {
-		if (i < 1 || i > count) throw new IndexOutOfBoundsException(s"index should be [1,$size) but is $i")
-		A(i-1)
-	}
-	final def left(i:Int): Int = i*2
-	final def right(i:Int): Int = i*2 + 1
-	final def parent(i: Int): Int = i/2
-	final def size: Int = count
-	final def head: N = A.head 
+/** The (binary) heap data structure is an array-wrapping object that can be viewed as a nearly complete binary tree */
+trait GenericHeap[@specialized(Int, Long, Double) N] extends Heap[N] {
+    self =>
 
-	final def insert(elem: N): Unit = {
-		A += elem
-		count = count + 1
-		bubbleUp(count)
-	}
-	
-	final def extract: Option[N] = {
-		if (count == 0) None
-		else {
-			val head = this(1)
-			swap(1,count)
-			count = count - 1
-			bubbleDown(1)
-			Some(head)
-		}
-	}
+    protected val shouldBeHigher: (N, N) => Boolean
+    protected val A: Buffer[N]
+    private val map: Map[N,Int] = Map()
 
-	private def update(i:Int,elem:N):Unit = {
-		A(i-1) = elem
-	}
-	
-	@tailrec
-	private def bubbleDown(i:Int): Unit = {
-		if (i > count) return
-		val j: Int = check(i,left(i),right(i))
-		if (j != i) { 
-	        swap(j,i)
-			bubbleDown(j)
-		}
-	}
+    protected var count: Int = 0
 
-	@tailrec
-	private def bubbleUp(i:Int): Unit = {
-		if (i <= 1) return
-		val p = parent(i)
-		val j: Int = check(p,left(p),right(p))
-		if (j != p) {
-			swap(j,p)
-			bubbleUp(p)
-		}
-	}
-	
-	private def check(i:Int,l:Int,r:Int): Int = {
-		if(l <= count && compare(this(l),this(i))) {
-			if(r <= count && compare(this(r),this(l))) r else l
-		} else {
-			if(r <= count && compare(this(r),this(i))) r else i
-		}
-	}
-	
-	private def swap(i:Int, j:Int): Unit = {
-		val e = this(i)
-		this(i) = this(j)
-		this(j) = e
-	}
+    @inline def apply(i: Int): N = A(i - 1)
+    @inline protected def update(i: Int, elem: N): Unit = { 
+        A(i - 1) = elem
+        map(elem) = i
+    }
+
+    @inline final def left(i: Int): Int = i * 2
+    @inline final def right(i: Int): Int = i * 2 + 1
+    @inline final def parent(i: Int): Int = i / 2
+
+    override final def size: Int = count
+    override final def head: N = A.head
+    
+    override def insert(elem: N): Unit = {
+        A += elem
+        count = count + 1
+        this(count) = elem
+        bubbleUp(count)
+    }
+
+    override def extract: Option[N] = {
+        if (count == 0) None
+        else {
+            val elem = head
+            swap(1, count)
+            count = count - 1
+            map.remove(elem)
+            bubbleDown(1)
+            Some(elem)
+        }
+    }
+
+    @tailrec
+    protected final def bubbleDown(i: Int): Unit = {
+        if (i > count) return
+        val j: Int = check(i, left(i), right(i))
+        if (j != i) {
+            swap(j, i)
+            bubbleDown(j)
+        }
+    }
+
+    @tailrec
+    protected final def bubbleUp(i: Int): Unit = {
+        if (i <= 1) return
+        val p = parent(i)
+        val j: Int = check(p, left(p), right(p))
+        if (j != p) {
+            swap(j, p)
+            bubbleUp(p)
+        }
+    }
+
+    @inline final def check(i: Int, l: Int, r: Int): Int = {
+        if (l <= count && shouldBeHigher(this(l), this(i))) {
+            if (r <= count && shouldBeHigher(this(r), this(l))) r else l
+        } else {
+            if (r <= count && shouldBeHigher(this(r), this(i))) r else i
+        }
+    }
+
+    @inline final def swap(i: Int, j: Int): Unit = {
+        val e = this(i)
+        this(i) = this(j)
+        this(j) = e
+    }
+
+    override def refresh(elem: N): Unit = {
+        for(i <- map.get(elem)){
+            if (shouldBeHigher(this(i),this(parent(i)))){
+                bubbleUp(i)
+            } else {
+                bubbleDown(i)
+            }
+        }
+    }
+
+    override def remove(elem: N): Unit = {
+        if (count == 0) return
+        for(i <- map.get(elem)){
+            swap(i, count)
+            count = count - 1
+            map.remove(elem)
+            bubbleDown(i)
+        }
+    }
 
 }
 
-class MinHeap[@specialized(Int,Long,Double) N : Ordering] extends Heap[N] {
-	val compare:(N,N) => Boolean = implicitly[Ordering[N]].lt
+class MinHeap[@specialized(Int, Long, Double) N: Ordering](initialSize: Int = 16) extends GenericHeap[N] {
+    override protected val A = new ArrayBuffer[N](initialSize)
+    override protected val shouldBeHigher: (N, N) => Boolean = implicitly[Ordering[N]].lt
 }
 
-class MaxHeap[@specialized(Int,Long,Double) N : Ordering] extends Heap[N] {
-	val compare:(N,N) => Boolean = implicitly[Ordering[N]].gt
+class MaxHeap[@specialized(Int, Long, Double) N: Ordering](initialSize: Int = 16) extends GenericHeap[N] {
+    override protected val A = new ArrayBuffer[N](initialSize)
+    override protected val shouldBeHigher: (N, N) => Boolean = implicitly[Ordering[N]].gt
 }
--- src/test/scala/HeapTest.scala
+++ src/test/scala/HeapTest.scala
@@ -2,16 +2,19 @@ import org.scalatest.FunSpec
 
 class HeapTest extends FunSpec {
 	
-	val heap = new MinHeap[Int]
-	heap insert 1221
-	heap insert 1101
-	heap insert 100
-	heap insert 2
-	heap insert 3
-	heap insert 999
+    def fixture = new {
+        val heap = new MinHeap[Int]
+        heap insert 1221
+        heap insert 1101
+        heap insert 100
+        heap insert 2
+        heap insert 3
+        heap insert 999
+    }
 
 	describe("Heap") {
 		it ("should calculate left, right and parent element index") {
+            val f = fixture; import f._
 			assert(heap.left(1)==2, s"left of 1 should be 2")
 			assert(heap.right(1)==3, s"right of 1 should be 3")
 			assert(heap.left(2)==4, s"left of 2 should be 4")
@@ -24,9 +27,11 @@ class HeapTest extends FunSpec {
 			assert(heap.parent(7)==3, s"parent of 7 should be 3")
 		}
 		it ("should insert element while maintaining heap property") {
+            val f = fixture; import f._
 			assert(heap.head==2, s"heap head should be 2 but is ${heap.head}")
 		}
 		it ("should extract head while maintaining heap property") {
+            val f = fixture; import f._
 			val elem = heap.extract.get
 			assert(elem==2, s"extracted should be 2 but is ${elem}")
 			assert(heap.head==3, s"heap head should be 3 but is ${heap.head}")
@@ -34,6 +39,17 @@ class HeapTest extends FunSpec {
 			assert(elem2==3, s"extracted should be 3 but is ${elem2}")
 			assert(heap.head==100, s"heap head should be 100 but is ${heap.head}")
 		}
+        it ("should remove element while maintaining heap property") {
+            val f = fixture; import f._
+            heap.remove(3)
+            heap.remove(999)
+            assert(heap.head==2, s"heap head should be 2 but is ${heap.head}")
+            assert(heap.size==4, s"heap size should be 4 but is ${heap.size}")
+            heap.extract
+            assert(heap.head==100, s"heap head should be 100 but is ${heap.head}")
+            heap.extract
+            assert(heap.head==1101, s"heap head should be 1101 but is ${heap.head}")
+        }
 	}
 	
 }
